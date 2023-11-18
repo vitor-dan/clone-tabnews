624bcc78b2f20179564165f3a47e9ff6abbccf64
vitor-dan <53277359+vitor-dan@users.noreply.github.com>
2023-11-18 19:21:57 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Te amo demais Flavinha!</h1>
+    return (
+        <>
+            <h1>Te amo demais Flavinha!</h1>
+            <p>Saiba que eu quero demais te fazer muito feliz!</p>
+        </>
+    )
 }
 
 export default Home;
