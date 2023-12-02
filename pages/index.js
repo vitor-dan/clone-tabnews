59e2ede3cb62647d3476f4de7229a07f330af7a4
vitor-dan <53277359+vitor-dan@users.noreply.github.com>
2023-12-02 02:13:05 +0000

    feat: atualizad mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Te amo demais Flavinha!</h1>
+      <h1>Vamos morar junto?</h1>
       <p>Saiba que eu quero demais te fazer muito feliz!</p>
     </>
   );
