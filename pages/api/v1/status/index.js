326c0ab38ecf9a0fc891a5033905c407707e006c
vitor-dan <53277359+vitor-dan@users.noreply.github.com>
2024-05-06 20:14:08 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const version = await database.query("SHOW server_version");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  const databaseMaxConnections = await database.query("SHOW max_connections");
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: parseInt(databaseOpenedConnections.rows[0].count),
+      },
+    },
+  });
 }
 
 export default status;
